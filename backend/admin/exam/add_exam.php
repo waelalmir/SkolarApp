01c7df58642d1397371ef8f7fee7b4d7f25e27bc
Wael Almir <eng.waelalmir@gmail.com>
2025-12-21 23:49:23 +0300

    Delete backend directory

--- backend/admin/exam/add_exam.php
+++
@@ -1,47 +0,0 @@
-<?php
-include "../../connect.php"; // الاتصال بقاعدة البيانات واستدعاء الدوال
-
-// تحقق من المصادقة (اختياري إذا بدك تأمنها)
-checkAuthenticate();
-
-// 🧩 جلب البيانات من الطلب
-$gradeid        = filterRequest("gradeid");
-$subjectid        = filterRequest("subjectid");
-$examdate        = filterRequest( "examdate");
-$term        = filterRequest("term");// 1,2
-$examtype        = filterRequest("examtype"); //quiz/mid/final
-
-
-
-// ✅ التحقق من الحقول المطلوبة
-if (empty($subjectid) || empty($gradeid) || empty($examdate) || empty($term) || empty($examtype)) {
-    printFailure("الرجاء إدخال جميع الحقول المطلوبة (اسم المادة و اختيار الصف)");
-    exit;
-}
-
-
-
-// 🧩 تجهيز البيانات للإدخال
-$data = array(
-    "subject_id" => $subjectid,
-    "grade_id" => $gradeid,
-    "exam_date" => $examdate,
-    "term" => $term,
-    "exam_type" => $examtype,
-
-);
-
-// ✅ إدخال البيانات
-$count = insertData("exams", $data);
-
-// ✅ التحقق من النتيجة
-if ($count > 0) {
-    // استرجاع بيانات المستخدم الجديد لعرضها بعد الإضافة (اختياري)
-    $stmt = $con->prepare("SELECT * FROM exams");
-   
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    printSuccess($exam_type); // نجاح مع إرجاع البيانات
-} else {
-    printFailure("فشل في إضافة الامتحان");
-}
--- backend/admin/session/add_sessions.php
+++
@@ -1,52 +0,0 @@
-
-<?php
-include "../../connect.php"; // الاتصال بقاعدة البيانات واستدعاء الدوال
-
-// تحقق من المصادقة (اختياري إذا بدك تأمنها)
-checkAuthenticate();
-
-// 🧩 جلب البيانات من الطلب
-$gradeid        = filterRequest("gradeid");
-$sectionid        = filterRequest("sectionid");
-$subjectid        = filterRequest("subjectid");
-$teacherid        = filterRequest("teacherid");
-$dayofweek        = filterRequest("dayofweek");
-$starttime        = filterRequest("starttime");
-$endtime        = filterRequest("endtime");
-
-
-// ✅ التحقق من الحقول المطلوبة
-if (empty($sectionid) || empty($gradeid) ) {
-    printFailure("الرجاء إدخال جميع الحقول المطلوبة (اسم المادة و اختيار الصف)");
-    exit;
-}
-
-
-
-// 🧩 تجهيز البيانات للإدخال
-$data = array(
-    "grade_id" => $gradeid,
-    "section_id" => $sectionid,
-    "subject_id" => $subjectid,
-    "teacher_id" => $teacherid,
-    "day_of_week" => $dayofweek,
-    "start_time" => $starttime,
-    "end_time" => $endtime,
-
-);
-
-// ✅ إدخال البيانات
-$count = insertData("timetable", $data, false);
-
-// ✅ التحقق من النتيجة
-if ($count > 0) {
-    // استرجاع بيانات المستخدم الجديد لعرضها بعد الإضافة (اختياري)
-   $stmt = $con->prepare("SELECT * FROM timetable");
-$stmt->execute();
-$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-printSuccess($data);
-
-} else {
-    printFailure("فشل في إضافة الحصة ");
-}
