2c780e686ae8e8ef13001001f275a57304033c6c
Wael Almir <eng.waelalmir@gmail.com>
2025-12-22 21:57:28 +0300

    Update handlingdatacontroller.dart

--- lib/core/functions/handlingdatacontroller.dart
+++ lib/core/functions/handlingdatacontroller.dart
@@ -3,17 +3,13 @@
 import 'package:skolar/core/class/statusrequest.dart';
 
 StatusRequest handlingData(dynamic response) {
-  // response يأتي من دالة الـ fold في طبقة الـ data
-  // فإما أن يكون StatusRequest (في حالة الخطأ قبل الوصول للخادم)
-  // أو يكون Map (في حالة النجاح والوصول للخادم)
+
   if (response is StatusRequest) {
-    return response; // إذا كان خطأ من البداية (مثل انقطاع النت)، أرجعه مباشرة
+    return response; 
   } else {
-    // إذا وصل للخادم وأرجع بيانات، تحقق من status بداخلها
     if (response['status'] == "success") {
       return StatusRequest.success;
     } else {
-      // إذا كان الـ status من الخادم هو "fail" (فشل منطقي)
       return StatusRequest.failure;
     }
   }
